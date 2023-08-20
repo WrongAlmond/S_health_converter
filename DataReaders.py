1a8c80d9e90b5029ab5fccf877a6488c36a5a77f
WrongAlmond <WrongAlmond@anonym.com>
2023-08-20 16:54:27 +0200

    more-or less working now, needs fine-tuning

--- DataReaders.py
+++ DataReaders.py
@@ -3,33 +3,7 @@ import os
 
 
 def read_activities_excel(file_name):
-    raw_data_frame = pd.read_csv(file_name, skiprows=1, index_col=False)
-
-    col_select = ["live_data_internal",
-                  "total_calorie",
-                  "location_data_internal",
-                  "additional_internal",
-                  "com.samsung.health.exercise.duration",
-                  "com.samsung.health.exercise.location_data",
-                  "com.samsung.health.exercise.start_time",
-                  "com.samsung.health.exercise.exercise_type",
-                  "com.samsung.health.exercise.mean_heart_rate",
-                  "com.samsung.health.exercise.max_heart_rate",
-                  "com.samsung.health.exercise.create_time",
-                  "com.samsung.health.exercise.max_speed",
-                  "com.samsung.health.exercise.mean_cadence",
-                  "com.samsung.health.exercise.min_heart_rate",
-                  "com.samsung.health.exercise.distance",
-                  "com.samsung.health.exercise.calorie",
-                  "com.samsung.health.exercise.max_cadence",
-                  "com.samsung.health.exercise.vo2_max",
-                  "com.samsung.health.exercise.live_data",
-                  "com.samsung.health.exercise.mean_speed",
-                  "com.samsung.health.exercise.end_time",
-                  "com.samsung.health.exercise.datauuid",
-                  "com.samsung.health.exercise.sweat_loss"]
-
-    select_data = raw_data_frame[col_select].copy()
+    data_frame = pd.read_csv(file_name, skiprows=1, index_col=False)
 
     sport_types = {1001: "Walking",
                    11007: "Cycling",
@@ -43,12 +17,10 @@ def read_activities_excel(file_name):
                    15002: "Garmin_breathing_exc",
                    13002: "Rock_climbing"}
 
-    select_data["com.samsung.health.exercise.exercise_type"].replace(sport_types, inplace=True)
+    data_frame["com.samsung.health.exercise.exercise_type"].replace(sport_types, inplace=True)
+    data_frame.index = data_frame['com.samsung.health.exercise.datauuid']
 
-    for col in select_data.columns:
-        select_data.rename(columns={col: col.replace("com.samsung.health.exercise.", "")}, inplace=True)
-
-    return select_data
+    return data_frame
 
 
 def get_file_list(path):
@@ -77,28 +49,266 @@ def get_file_list(path):
     return pd.DataFrame(file_dicts).transpose()
 
 
-def read_json_data(path):
-    accepted_files = ["com.samsung.health.exercise.live_data.json",
-                      "com.samsung.health.exercise.location_data.json",
-                      "live_data_internal.json",
-                      "location_data_internal.json"]
+def read_json_for_tcx(file_db, test_id):
+    test_loc = file_db.index.get_loc(test_id)
+    path = file_db.iloc[test_loc, :]
 
-    compiled_frame = pd.read_json(path[accepted_files[0]])
-    # print(compiled_frame.columns)
+    used_files = ["com.samsung.health.exercise.live_data.json",
+                  "com.samsung.health.exercise.location_data.json",
+                  "live_data_internal.json",
+                  "location_data_internal.json"]
 
-    # for file in path:
-    #     # if not(pd.isna(file)) and "com.samsung.health.exercise.live_data.json" not in str(file) and "live" in str(file) and "location" in str(file):
-    #     if not (pd.isna(file)) and "com.samsung.health.exercise.live_data.json" not in str(file):
-    for i in range(1, len(accepted_files)):
-        path_to_read = path[accepted_files[i]]
-        if not(pd.isna(path_to_read)):
+    compiled_frame = pd.read_json(path[used_files[0]])
+
+    for i in range(1, len(used_files)):
+        path_to_read = path[used_files[i]]
+        if not (pd.isna(path_to_read)):
             my_json = pd.read_json(path_to_read)
             compiled_frame = pd.merge(compiled_frame, my_json, on="start_time", how="outer")
 
     compiled_frame.sort_values(by="start_time", inplace=True)
 
+    columns_to_drop = ['percent_of_vo2max', 'accuracy', 'elapsed_time_x',
+                       'interval_x', 'segment_x', 'elapsed_time_y',
+                       'interval_y', 'segment_y']
+
+    for col in compiled_frame:
+        if col in columns_to_drop:
+            compiled_frame.drop([col], axis='columns', inplace=True)
+
+    # compiled_frame = compiled_frame.resample("2S", on='start_time').mean()
+    #
+    # compiled_frame['start_time'] = compiled_frame.index
     # compiled_frame.to_csv("test.csv")
 
-    # raw_data = pd.read_json(path)
-    # print((raw_data))
     return compiled_frame
+
+
+def get_tcx_header_data(df, data_id):
+    if data_id in df.index:
+        row_id = df.index.get_loc(data_id)
+        row_data = df.iloc[row_id, :]
+
+        # header data
+        sport_type = row_data['com.samsung.health.exercise.exercise_type']
+        start_time = row_data['com.samsung.health.exercise.start_time']
+        total_time_sec = row_data['com.samsung.health.exercise.duration'] / 1000  # original data in ms
+        distance_meters = row_data['com.samsung.health.exercise.distance']
+        max_speed = row_data['com.samsung.health.exercise.max_speed']
+        calories = row_data['com.samsung.health.exercise.calorie']
+        max_hr = row_data['com.samsung.health.exercise.max_heart_rate']
+        mean_hr = row_data['com.samsung.health.exercise.mean_heart_rate']
+        mean_cadence = row_data['com.samsung.health.exercise.mean_cadence']
+        max_cadence = row_data['com.samsung.health.exercise.max_cadence']
+        mean_speed = row_data['com.samsung.health.exercise.mean_speed']
+
+        header_data = [sport_type, start_time, total_time_sec, distance_meters, max_speed, calories, max_hr, mean_hr,
+                       mean_cadence, max_cadence, mean_speed]
+
+        # for i in range(len(header_data)):
+        #     if pd.isna(header_data[i]):
+        #         header_data[i] = 0
+        #         print("NAN FOUND")
+
+        return header_data
+    else:
+        return False
+
+
+def create_tcx_header(header_data):
+    #  header_data = [sport_type, start_time, total_time_sec, distance_meters, max_speed, calories, max_hr, mean_hr,
+    #                    mean_cadence, max_cadence]
+    header_data[1] = f"{header_data[1].replace(' ', 'T')}"
+    header = (
+        f'<?xml version="1.0" encoding="UTF-8"?>\n'
+        f'<TrainingCenterDatabase\n'
+        f'  xsi:schemaLocation="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd"\n'
+        f'  xmlns:ns5="http://www.garmin.com/xmlschemas/ActivityGoals/v1"\n'
+        f'  xmlns:ns3="http://www.garmin.com/xmlschemas/ActivityExtension/v2"\n'
+        f'  xmlns:ns2="http://www.garmin.com/xmlschemas/UserProfile/v2"\n'
+        f'  xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"\n'
+        f'  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ns4="http://www.garmin.com/xmlschemas/ProfileExtension/v1">\n'
+        f'  <Activities>\n'
+        f'    <Activity Sport="{header_data[0]}">\n'
+        f'      <Id>{header_data[1]}Z</Id>\n'
+        f'      <Lap StartTime="{header_data[1]}Z">\n'
+
+    )
+
+    if not (pd.isna(header_data[2])):
+        header += (
+            f'        <TotalTimeSeconds>{header_data[2]}</TotalTimeSeconds>\n')
+
+    if not (pd.isna(header_data[3])):
+        header += (
+            f'        <DistanceMeters>{header_data[3]}</DistanceMeters>\n')
+
+    if not (pd.isna(header_data[4])):
+        header += (
+            f'        <MaximumSpeed>{header_data[4]}</MaximumSpeed>\n')
+
+    if not (pd.isna(header_data[5])):
+        header += (
+            f'        <Calories>{int(header_data[5])}</Calories>\n')
+
+    if not (pd.isna(header_data[7])):
+        header += (
+            f'        <AverageHeartRateBpm>\n'
+            f'          <Value>{int(header_data[7])}</Value>\n'
+            f'        </AverageHeartRateBpm>\n')
+
+    if not (pd.isna(header_data[6])):
+        header += (
+            f'        <MaximumHeartRateBpm>\n'
+            f'          <Value>{int(header_data[6])}</Value>\n'
+            f'        </MaximumHeartRateBpm>\n')
+
+    header += (
+        f'        <Intensity>Active</Intensity>\n'
+        f'        <TriggerMethod>Manual</TriggerMethod>\n')
+
+    return header
+
+
+def create_tcx_track_points(input_track_points):
+    # ['heart_rate', 'start_time', 'cadence', 'distance', 'speed', 'altitude', 'latitude', 'longitude', 'cadence']
+    cols = input_track_points.columns
+
+    if len(input_track_points) > 0:
+        data_out = f"        <Track>\n"
+        distance = 0.0
+
+        for index, row in input_track_points.iterrows():
+            time_stamp = f"{str(row['start_time']).replace(' ', 'T')}Z"
+
+            data_out += f"          <Trackpoint>\n"
+            data_out += (
+                f"            <Time>{time_stamp}</Time>\n"
+            )
+
+            if "latitude" in cols and "longitude" in cols:
+                if not (pd.isna(row["latitude"])) and not (pd.isna(row["longitude"])):
+                    data_out += (
+                        f"            <Position>\n"
+                        f"              <LatitudeDegrees>{row['latitude']}</LatitudeDegrees>\n"
+                        f"              <LongitudeDegrees>{row['longitude']}</LongitudeDegrees>\n"
+                        f"            </Position>\n"
+                    )
+
+            if "altitude" in cols:
+                if not (pd.isna(row["altitude"])):
+                    data_out += (
+                        f"            <AltitudeMeters>{row['altitude']}</AltitudeMeters>\n"
+                    )
+
+            if "distance" in cols:
+                if not (pd.isna(row["distance"])):
+                    distance += row['distance']
+                    data_out += (
+                        f"            <DistanceMeters>{distance}</DistanceMeters>\n"
+                    )
+
+            if "heart_rate" in cols:
+                if not (pd.isna(row["heart_rate"])):
+                    data_out += (
+                        f"            <HeartRateBpm>\n"
+                        f"              <Value>{int(row['heart_rate'])}</Value>\n"
+                        f"            </HeartRateBpm>\n"
+                    )
+
+            if "speed" in cols and 'cadence' in cols:
+                if not (pd.isna(row["speed"])) and not (pd.isna(row["cadence"])):
+                    data_out += (
+                        f"            <Extensions>\n"
+                        f"              <ns3:TPX>\n"
+                        f"                <ns3:Speed>{row['speed']}</ns3:Speed>\n"
+                        f"                <ns3:RunCadence>{row['cadence']}</ns3:RunCadence>\n"
+                        f"              </ns3:TPX>\n"
+                        f"            </Extensions>\n"
+                    )
+            elif "speed" in cols:
+                if not (pd.isna(row["speed"])):
+                    data_out += (
+                        f"            <Extensions>\n"
+                        f"              <ns3:TPX>\n"
+                        f"                <ns3:Speed>{row['speed']}</ns3:Speed>\n"
+                        f"              </ns3:TPX>\n"
+                        f"            </Extensions>\n"
+                    )
+
+            elif "cadence" in cols:
+                if not (pd.isna(row["cadence"])):
+                    data_out += (
+                        f"            <Extensions>\n"
+                        f"              <ns3:TPX>\n"
+                        f"                <ns3:RunCadence>{row['cadence']}</ns3:RunCadence>\n"
+                        f"              </ns3:TPX>\n"
+                        f"            </Extensions>\n"
+                    )
+
+            data_out += f"          </Trackpoint>\n"
+        data_out += f"        </Track>\n"
+
+        return data_out
+
+
+def create_tcx_footer(header_data):
+    # header_data = [sport_type, start_time, total_time_sec, distance_meters, max_speed, calories, max_hr, mean_hr,
+    #                mean_cadence, max_cadence, mean_speed]
+    footer_data = ""
+
+    if not (pd.isna(header_data[10])) or not (pd.isna(header_data[8])) or not (pd.isna(header_data[9])):
+        footer_data += (
+            f"    <Extensions>\n"
+            f"      <ns3:LX>\n")
+    if not (pd.isna(header_data[10])):
+        footer_data += (
+            f"        <ns3:AvgSpeed>{header_data[10]}</ns3:AvgSpeed>\n")
+    if not (pd.isna(header_data[8])):
+        footer_data += (
+            f"        <ns3:AvgRunCadence>{header_data[8]}</ns3:AvgRunCadence>\n")
+    if not (pd.isna(header_data[9])):
+        footer_data += (
+            f"        <ns3:MaxRunCadence>{header_data[9]}</ns3:MaxRunCadence>\n")
+    if not (pd.isna(header_data[10])) or not (pd.isna(header_data[8])) or not (pd.isna(header_data[9])):
+        footer_data += (
+            f"      </ns3:LX>\n"
+            f"    </Extensions>\n")
+
+    footer_data += (
+        f"    </Lap>\n"
+        f'    <Creator xsi:type="Device_t">\n'
+        f"      <Name> Samsung Galaxy Watch</Name>\n"
+        f"      <UnitId>0000000000</UnitId>\n"
+        f"      <ProductID>0000</ProductID>\n"
+        f"      <Version>\n"
+        f"        <VersionMajor>0</VersionMajor>\n"
+        f"        <VersionMinor> 0 </VersionMinor>\n"
+        f"        <BuildMajor>0</BuildMajor>\n"
+        f"        <BuildMinor>0</BuildMinor>\n"
+        f"      </Version>\n"
+        f"    </Creator>\n"
+        f"  </Activity>\n"
+        f"</Activities>\n"
+        f'<Author xsi:type="Application_t">\n'
+        f"  <Name>Connect Api</Name>\n"
+        f"  <Build>\n"
+        f"    <Version>\n"
+        f"      <VersionMajor>0</VersionMajor>\n"
+        f"      <VersionMinor>0</VersionMinor>\n"
+        f"      <BuildMajor>0</BuildMajor>\n"
+        f"      <BuildMinor>0</BuildMinor>\n"
+        f"    </Version>\n"
+        f"  </Build>\n"
+        f"  <LangID>en</LangID>\n"
+        f"  <PartNumber>000-00000-00</PartNumber>\n"
+        f"</Author>\n"
+        f"</TrainingCenterDatabase>\n")
+
+    return footer_data
+
+
+def save_tcx(header, json_data, footer, activity, id_no):
+    data = header + json_data + footer
+    with open(f'data_exports/{activity}_{id_no}.tcx', 'w+') as f:
+        f.write(data)
--- main.py
+++ main.py
@@ -1,26 +1,73 @@
 import pandas as pd
-import os
-from DataReaders import read_activities_excel, get_file_list, read_json_data
+from DataReaders import read_activities_excel, get_file_list, read_json_for_tcx, get_tcx_header_data, \
+                        create_tcx_header, create_tcx_track_points, create_tcx_footer, save_tcx
 
 my_excel = 'Samsung_Health_exports\com.samsung.shealth.exercise.20230813165934.csv'
 jsons = 'Samsung_Health_exports\jsons\com.samsung.shealth.exercise'
 
+# reading main csv containing summary of exercise data
 main_df = read_activities_excel(my_excel)
 
+# getting list of json files containing exercise info
 file_db = get_file_list(jsons)
-data_ids = pd.Series(file_db.index)
 
-test_loc = file_db.index.get_loc('4ac7acfe-875c-4108-92a6-1c5ecc3fdaff')
-test_path = file_db.iloc[test_loc, :]
-read_json_data(test_path)
 
+# #### Start of working stuff
 #
-# for ids in data_ids:
-#     test_loc = file_db.index.get_loc(ids)
-#     test_path = file_db.iloc[test_loc, :]
-#     print(ids)
-#     read_json_data(test_path)
+# test_id = '4ac7acfe-875c-4108-92a6-1c5ecc3fdaff'
+#
+# # reading json for selected ID
+# json_data = read_json_for_tcx(file_db, test_id)
+#
+# # reading data for creating tcx header
+# header_data = get_tcx_header_data(main_df, test_id)
+#
+# # creating tcx
+# header = create_tcx_header(header_data)
+# body = create_tcx_track_points(json_data)
+# footer = create_tcx_footer(header_data)
+# save_tcx(header, body, footer, header_data[0], test_id)
+#
+# ## end of working stuff
+#
+#
+# data_ids = pd.Series(file_db.index)
+# failed_files = []
+# for i in range(0, len(data_ids)):
+#     # reading json for selected ID
+#     json_data = read_json_for_tcx(file_db, data_ids[i])
+#
+#     # reading data for creating tcx header
+#     header_data = get_tcx_header_data(main_df, data_ids[i])
+#     if header_data:
+#         # creating tcx
+#         header = create_tcx_header(header_data)
+#         body = create_tcx_track_points(json_data)
+#         footer = create_tcx_footer(header_data)
+#         save_tcx(header, body, footer, header_data[0], data_ids[i])
+#
+#         print(f"progress: {round(i/len(data_ids),4)*100}%")
+#     else:
+#         print(f"no info found for {data_ids[i]} in main csv")
+#         failed_files.append(data_ids[i])
 
 
-# test_path = pd.Series(file_db[file_db.index == '4ac7acfe-875c-4108-92a6-1c5ecc3fdaff'])
-# read_json_data(test_path)
+#### Start of working stuff 2
+
+test_id = 'd81b1b76-bcad-409f-a1e3-f4f953d01a78'
+
+# reading json for selected ID
+json_data = read_json_for_tcx(file_db, test_id)
+
+# reading data for creating tcx header
+header_data = get_tcx_header_data(main_df, test_id)
+
+if header_data:
+    # creating tcx
+    header = create_tcx_header(header_data)
+    body = create_tcx_track_points(json_data)
+    footer = create_tcx_footer(header_data)
+    save_tcx(header, body, footer, header_data[0], test_id)
+
+
+## end of working stuff
